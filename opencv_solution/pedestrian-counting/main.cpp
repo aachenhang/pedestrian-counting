a49756c9269f03cb4db6e7fa61b30de2810a364a
aachenhang <631150134@qq.com>
2017-01-05 20:51:33 +0800

    add matlab extern interface for reading .mat file

--- opencv_solution/pedestrian-counting/main.cpp
+++ opencv_solution/pedestrian-counting/main.cpp
@@ -19,28 +19,131 @@
 
 using namespace cv;
 using namespace std;
+#include <mat.h>
+#include "mxAnalyze.h"
+int diagnose(const char *file) {
+	MATFile *pmat;
+	const char **dir;
+	const char *name;
+	int	  ndir;
+	int	  i;
+	mxArray *pa;
 
-//hide the local functions in an anon namespace
-namespace {
-	void help(char** av) {
-		cout << "The program captures frames from a video file, image sequence (01.jpg, 02.jpg ... 10.jpg) or camera connected to your computer." << endl
-			<< "Usage:\n" << av[0] << " <video file, image sequence or device number>" << endl
-			<< "q,Q,esc -- quit" << endl
-			<< "space   -- save frame" << endl << endl
-			<< "\tTo capture from a camera pass the device number. To find the device number, try ls /dev/video*" << endl
-			<< "\texample: " << av[0] << " 0" << endl
-			<< "\tYou may also pass a video file instead of a device number" << endl
-			<< "\texample: " << av[0] << " video.avi" << endl
-			<< "\tYou can also pass the path to an image sequence and OpenCV will treat the sequence just like a video." << endl
-			<< "\texample: " << av[0] << " right%%02d.jpg" << endl;
+	printf("Reading file %s...\n\n", file);
+
+	/*
+	* Open file to get directory
+	*/
+	pmat = matOpen(file, "r");
+	if (pmat == NULL) {
+		printf("Error opening file %s\n", file);
+		return(1);
 	}
 
+	/*
+	* get directory of MAT-file
+	*/
+	dir = (const char **)matGetDir(pmat, &ndir);
+	if (dir == NULL) {
+		printf("Error reading directory of file %s\n", file);
+		return(1);
+	}
+	else {
+		printf("Directory of %s:\n", file);
+		for (i = 0; i < ndir; i++)
+			printf("%s\n", dir[i]);
+	}
+	mxFree(dir);
+
+	/* In order to use matGetNextXXX correctly, reopen file to read in headers. */
+	if (matClose(pmat) != 0) {
+		printf("Error closing file %s\n", file);
+		return(1);
+	}
+	pmat = matOpen(file, "r");
+	if (pmat == NULL) {
+		printf("Error reopening file %s\n", file);
+		return(1);
+	}
+
+	/* Get headers of all variables */
+	printf("\nExamining the header for each variable:\n");
+	for (i = 0; i < ndir; i++) {
+		pa = matGetNextVariableInfo(pmat, &name);
+		if (pa == NULL) {
+			printf("Error reading in file %s\n", file);
+			return(1);
+		}
+		/* Diagnose header pa */
+		printf("According to its header, array %s has %d dimensions\n",
+			name, mxGetNumberOfDimensions(pa));
+		if (mxIsFromGlobalWS(pa))
+			printf("  and was a global variable when saved\n");
+		else
+			printf("  and was a local variable when saved\n");
+		mxDestroyArray(pa);
+	}
+
+	/* Reopen file to read in actual arrays. */
+	if (matClose(pmat) != 0) {
+		printf("Error closing file %s\n", file);
+		return(1);
+	}
+	pmat = matOpen(file, "r");
+	if (pmat == NULL) {
+		printf("Error reopening file %s\n", file);
+		return(1);
+	}
+
+	/* Read in each array. */
+	printf("\nReading in the actual array contents:\n");
+	for (i = 0; i<ndir; i++) {
+		pa = matGetNextVariable(pmat, &name);
+		if (pa == NULL) {
+			printf("Error reading in file %s\n", file);
+			return(1);
+		}
+		/*
+		* Diagnose array pa
+		*/
+		printf("According to its contents, array %s has %d dimensions\n",
+			name, mxGetNumberOfDimensions(pa));
+		if (mxIsFromGlobalWS(pa))
+			printf("  and was a global variable when saved\n");
+		else
+			printf("  and was a local variable when saved\n");
+
+		if (mxIsCell(pa)) {
+			cout << "is cell" << endl;
+		}
+		else if (mxIsStruct(pa)) {
+			cout << "is struct" << endl;
+		}
+		else if (mxIsNumeric(pa)) {
+			cout << "is numeric" << endl;
+		}
+		else {
+			cout << "is others" << endl;
+		}
+
+		analyze_full(pa);
+
+		mxDestroyArray(pa);
+	}
+
+
+	if (matClose(pmat) != 0) {
+		printf("Error closing file %s\n", file);
+		return(1);
+	}
+	printf("Done\n");
+	return(0);
+}
+//hide the local functions in an anon namespace
+namespace {
 	int process(VideoCapture& capture) {
 		int n = 0;
 		char filename[200];
-		string window_name = "video | q or esc to quit";
-		cout << "press space to save a picture. q or esc to quit" << endl;
-		//namedWindow(window_name, WINDOW_KEEPRATIO); //resizable window;
 		Mat frame;
 		
 		/*capture >> frame;
@@ -56,11 +159,11 @@ namespace {
 */
 		int i = 0;
 		double t = (double)getTickCount();
-		for (;;) {
-			capture >> frame;
-			printf("%d\n", i++);
-			if (frame.empty())
-				break;
+		//for (;;) {
+		//	capture >> frame;
+		//	printf("%d\n", i++);
+		//	if (frame.empty())
+		//		break;
 
 			//imshow(window_name, frame);
 			//char key = (char)waitKey(30); //delay N millis, usually long enough to display and capture input
@@ -79,33 +182,44 @@ namespace {
 			//	cout << "Default " << filename << endl;
 			//	break;
 			//}
-		}
+		//}
 
-		t = ((double)getTickCount() - t) / getTickFrequency();
-		cout << "Times passed in seconds: " << t << endl;//265.83s-33800frames
+		//t = ((double)getTickCount() - t) / getTickFrequency();
+		//cout << "Times passed in seconds: " << t << endl;//265.83s-33800frames
 		return 0;
 	}
 }
 
-int main(int ac, char** av) {
-	cv::CommandLineParser parser(ac, av, "{help h||}{@input||}");
-	if (parser.has("help"))
-	{
-		help(av);
-		return 0;
-	}
-	//std::string arg = parser.get<std::string>("@input");
-	std::string arg = "F:/Downloads/8116_IP_01_20130224173726_20130224180008_2057515_0001.mp4";
+int main(int argc, char** argv) {
+	//cv::CommandLineParser parser(ac, av, "{help h||}{@input||}");
+	//if (parser.has("help"))
+	//{
+	//	return 0;
+	//}
+	////std::string arg = parser.get<std::string>("@input");
+	//std::string arg = "F:/Downloads/8116_IP_01_20130224173726_20130224180008_2057515_0001.mp4";
 
-	if (arg.empty()) {
-		help(av);
-		return 1;
+	//if (arg.empty()) {
+	//	return 1;
+	//}
+	//VideoCapture capture(arg); //try to open string, this will attempt to open it as a video file or image sequence
+	//if (!capture.isOpened()) {
+	//	cerr << "Failed to open the video device, video file or image sequence!\n" << endl;
+	//	return 1;
+	//}
+	//return process(capture);
+	int result;
+
+	char matfile[] = "F:/Downloads/Compressed/mall_dataset/mall_feat.mat";
+	argc = 2;
+	if (argc > 1)
+		result = diagnose(matfile);
+	else {
+		result = 0;
+		printf("Usage: matdgns <matfile>");
+		printf(" where <matfile> is the name of the MAT-file");
+		printf(" to be diagnosed\n");
 	}
-	VideoCapture capture(arg); //try to open string, this will attempt to open it as a video file or image sequence
-	if (!capture.isOpened()) {
-		cerr << "Failed to open the video device, video file or image sequence!\n" << endl;
-		help(av);
-		return 1;
-	}
-	return process(capture);
+
+	return (result == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
